5bd757bbe8353f8961059e6d9258967968d0a150
Plushkin <61452848+plushkinv@users.noreply.github.com>
2023-07-28 12:39:57 +0500

    обновление контракта для минта черепов

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ proxy_changeIPlink = "http:/5cce3b204"
 
 
 #то что ниже желательно настроить под себя
-NFT_adress = "0xe5325804d68033edf65a86403b2592a99e1f06de"
+NFT_adress = "0x61b2d56645d697ac3a27c2fa1e5b26b45429d1a9"
 count_nfts = 1  # количество сколько хотите сминтить
 network4mint = ['avax', 'polygon', 'bsc', 'arbitrum', 'optimism']  # выберите сети, в одной из них будет случайно сминчена НФТ
 network4bridge = ['avax', 'polygon', 'bsc', 'arbitrum', 'optimism']  # выберите сети, в одну из них будет будет отправлена НФТ через мост
